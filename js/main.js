733776983f9c25f3f17158d8cdb8bf6b7ba76965
igorbr <Igor.Bronovytskyi@playtech.com>
2018-01-28 14:11:43 +0200

    refactoring

--- js/main.js
+++ js/main.js
@@ -1,20 +1,8 @@
 window.addEventListener('load', function() {
-    var click = document.querySelectorAll('dt');
-    var body = document.querySelectorAll('dd');
-
-    click[0].addEventListener('click', function () {
-        body[0].classList.toggle('open')
-    });
-
-    click[1].addEventListener('click', function () {
-        body[1].classList.toggle('open')
-    });
-
-    click[2].addEventListener('click', function () {
-        body[2].classList.toggle('open')
-    });
-
-    click[3].addEventListener('click', function () {
-        body[3].classList.toggle('open')
+    document.querySelector('.module--faq').addEventListener('click', function(event) {
+        var el = event.target.parentNode;
+        if (el.tagName === 'DT') {
+            el.classList.toggle('open');
+        }
     });
 });
